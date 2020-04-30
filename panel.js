20fc39fb81c8840161c223c9aff7d829ead2b19e
Tanvi <tkarandikar@gmail.com>
2020-04-30 17:09:26 +0530

    Showing time of the day

--- panel.js
+++ panel.js
@@ -382,6 +382,14 @@ function parseDate(d){
     return `${da}-${mo}-${ye}`;
 }
 
+function parseDateSlash(d){
+    const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
+    const mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(d)
+    const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d)
+
+    return `${da}/${mo}/${ye}`;
+}
+
 function timeFormat(v){
     var ifn=parseInt(v);
     return `${ifn}:00`
